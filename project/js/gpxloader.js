0661bf1e559d3bb3cadc1ef19a0d974c55923382
Martina Cocco <2267320c@student.gla.ac.uk>
2018-11-23 11:28:00 +0000

    Added mapboxGL layer to map. Changed padding on index page. Removed unneccessary contact buttons

--- project/js/gpxloader.js
+++ project/js/gpxloader.js
@@ -1,43 +1,57 @@
 var gpxFiles = ["https://raw.githubusercontent.com/martinacocco/ISproject/master/project/GPX%20Files/bartour.gpx",
-"https://raw.githubusercontent.com/martinacocco/ISproject/master/project/GPX%20Files/citycentremuseumtour.gpx",
-"https://raw.githubusercontent.com/martinacocco/ISproject/master/project/GPX%20Files/glasgowmust.gpx",
-"https://raw.githubusercontent.com/martinacocco/ISproject/master/project/GPX%20Files/westendtour.gpx"];
+    "https://raw.githubusercontent.com/martinacocco/ISproject/master/project/GPX%20Files/citycentremuseumtour.gpx",
+    "https://raw.githubusercontent.com/martinacocco/ISproject/master/project/GPX%20Files/glasgowmust.gpx",
+    "https://raw.githubusercontent.com/martinacocco/ISproject/master/project/GPX%20Files/westendtour.gpx"
+];
 
 var mymap;
 
-function initMap(){
-  mymap = L.map('map').setView([55.86515, -4.25763], 13);
-  L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="http://www.osm.org">OpenStreetMap</a>'
-  }).addTo(mymap);
+function initMap() {
+
+    //mymap = L.map('map').setView([55.86515, -4.25763], 13);
+    //L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    //    attribution: 'Map data &copy; <a href="http://www.osm.org">OpenStreetMap</a>'
+    //}).addTo(mymap);
+
+    var token ="pk.eyJ1IjoibWNvY2NvIiwiYSI6ImNqbjYwbDI1ZDE0ejEzdnJ1cG15NDJ4cXIifQ.duXi7u04FfnKiBRjlKpSIw"; // replace with your Mapbox API Access token. Create a Mabpox account and find it on https://www.mapbox.com/studio/
+
+    mymap = L.map('map').setView([55.857161,-4.249356], 13);
+
+    var gl = new L.mapboxGL({
+        accessToken: token,
+        style: 'mapbox://styles/mcocco/cjosz1lfb2o8q2skfx2mvrloa'
+    }).addTo(mymap);
+
 }
 
-function displayGPX(i){
-  clearMap();
-  var url = gpxFiles[i];
-  new L.GPX(url,{async: true}).on('loaded', function(e) {
-    mymap.fitBounds(e.target.getBounds());
-  }).addTo(mymap);
+function displayGPX(i) {
+    clearMap();
+    var url = gpxFiles[i];
+    new L.GPX(url, {
+        async: true
+    }).on('loaded', function(e) {
+        mymap.fitBounds(e.target.getBounds());
+    }).addTo(mymap);
 }
 
 function clearMap() {
-  mymap.eachLayer(function (layer) {
-    if(layer instanceof L.Marker) {
-      mymap.removeLayer(layer);
-    }
-    if (layer instanceof L.Polyline){
-      mymap.removeLayer(layer);
-    }
-  });
+    mymap.eachLayer(function(layer) {
+        if (layer instanceof L.Marker) {
+            mymap.removeLayer(layer);
+        }
+        if (layer instanceof L.Polyline) {
+            mymap.removeLayer(layer);
+        }
+    });
 }
 
 function uploadXML() {
     var regex = /^([a-zA-Z0-9\s_\\.\-:])+(.gpx)$/;
     //Checks whether the file is a valid xml file
     if (regex.test($("#xmlFile").val())) {
-        var name = $("#xmlFile").val().replace(".gpx","").replace("C:\\fakepath\\", "");
+        var name = $("#xmlFile").val().replace(".gpx", "").replace("C:\\fakepath\\", "");
         //Checks whether the browser supports HTML5
-        if (typeof (FileReader) != "undefined") {
+        if (typeof(FileReader) != "undefined") {
             var reader = new FileReader();
             reader.onload = function (e) {
                 try{
@@ -62,32 +76,32 @@ function uploadXML() {
     }
 }
 
-function addToSidebar(index, name){
+function addToSidebar(index, name) {
     $("#user-tours").append("<li id=tour-" + index + " class='list-group-item tours-list'>" + name + "</li>");
-    $(".tours-list").click(function(){
+    $(".tours-list").click(function() {
         var id = $(this).attr("id");
-        var index = parseInt(id.replace("tour-", ""),10);
+        var index = parseInt(id.replace("tour-", ""), 10);
         displayGPX(index);
     });
 }
 // Main Function From where to execute all js code
-$(function(){
+$(function() {
     initMap();
 
-    $(".tours-list").click(function(){
+    $(".tours-list").click(function() {
         var id = $(this).attr("id");
-        var index = parseInt(id.replace("tour-", ""),10);
+        var index = parseInt(id.replace("tour-", ""), 10);
 
         displayGPX(index);
         $(this).next().slideToggle(300);
-        for(var i = 0; i < gpxFiles.length; i++){
-            if(i != index && $("#tour-" + i + "-Description").is(":visible")){
+        for (var i = 0; i < gpxFiles.length; i++) {
+            if (i != index && $("#tour-" + i + "-Description").is(":visible")) {
                 $("#tour-" + i + "-Description").slideToggle(300);
             }
         }
     });
 
-    $('#xmlForm').submit(function (event) {
+    $('#xmlForm').submit(function(event) {
         event.preventDefault();
         uploadXML();
     });
